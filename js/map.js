0ca68545e7cf814bf2eaaf6fa12097eb79697bdb
Ricardo M Oliveira <rmartine@rmartine.gru.redhat.com>
2017-12-29 11:50:04 -0200

    Adiciona dados de criminalidade de 2003 e remove os de 2017

--- js/map.js
+++ js/map.js
@@ -4,12 +4,14 @@ var mymap = L.map('mapid', {
 });
 
 var overlayMaps = {};
-var homicidios = L.markerClusterGroup();
-var furtosCelular = L.markerClusterGroup();
-var roubosVeiculo = L.markerClusterGroup();
-var furtosVeiculo = L.markerClusterGroup();
-var latrocinios = L.markerClusterGroup();
-var lesaoMorte = L.markerClusterGroup();
+var furtosCelular	= L.markerClusterGroup();
+var furtosVeiculo	= L.markerClusterGroup();
+var homicidios		= L.markerClusterGroup();
+var latrocinios		= L.markerClusterGroup();
+var lesaoMorte		= L.markerClusterGroup();
+var mortesPolicial	= L.markerClusterGroup();
+var roubosCelular	= L.markerClusterGroup();
+var roubosVeiculo	= L.markerClusterGroup();
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	maxZoom: 18,
@@ -28,21 +30,51 @@ function eachLayer(layer) {
 		layer.bindPopup(text);
 	}
 	// Related doc: http://leafletjs.com/reference-1.1.0.html#icon
-	if(feature.properties.tipo == "Homicidio") {
+	if(feature.properties.tipo == "Furto de Celular") {
 		layer.options.icon = L.icon({
-			iconUrl: "/mapa-crime-sp/img/homicidio.png",
-			iconSize: [28, 45]
-		});
-		homicidios.addLayer(layer);
-	} else if(feature.properties.tipo == "Furto de Celular") {
-		layer.options.icon = L.icon({
-			iconUrl: "/mapa-crime-sp/img/furto.png",
+			iconUrl: "./img/furto.png",
 			iconSize: [28, 45]
 		});
 		furtosCelular.addLayer(layer);
+	} else if(feature.properties.tipo == "Furto de Veículo") {
+		layer.options.icon = L.icon({
+			iconUrl: "./img/furtoveiculo.png",
+			iconSize: [28, 45]
+		});
+		furtosVeiculo.addLayer(layer);
+	} else if(feature.properties.tipo == "Homicídio") {
+		layer.options.icon = L.icon({
+			iconUrl: "./img/homicidio.png",
+			iconSize: [28, 45]
+		});
+		homicidios.addLayer(layer);
+	} else if(feature.properties.tipo == "Latrocínio") {
+		layer.options.icon = L.icon({
+			iconUrl: "./img/latrocinio.jpg",
+			iconSize: [28, 45]
+		});
+		latrocinios.addLayer(layer);
+	} else if(feature.properties.tipo == "Lesão Seguida de Morte") {
+		layer.options.icon = L.icon({
+			iconUrl: "./img/lesaomorte.png",
+			iconSize: [28, 45]
+		});
+		lesaoMorte.addLayer(layer);
+	} else if(feature.properties.tipo == "Morte por Intervenção Policial") {
+		layer.options.icon = L.icon({
+			iconUrl: "./img/mortepolicial.png",
+			iconSize: [28, 45]
+		});
+		mortesPolicial.addLayer(layer);
+	} else if(feature.properties.tipo == "Roubo de Celular") {
+		layer.options.icon = L.icon({
+			iconUrl: "./img/roubocelular.png",
+			iconSize: [28, 45]
+		});
+		roubosCelular.addLayer(layer);
 	} else if(feature.properties.tipo == "Roubo de Veiculo") {
 		layer.options.icon = L.icon({
-			iconUrl: "/mapa-crime-sp/img/furtoveiculo.png",
+			iconUrl: "./img/rouboveiculo.png",
 			iconSize: [28, 45]
 		});
 		roubosVeiculo.addLayer(layer);
@@ -51,12 +83,14 @@ function eachLayer(layer) {
 
 var points = omnivore.csv("/mapa-crime-sp/data/massa.csv").on('ready', function () {
 	points.eachLayer(eachLayer);
-	overlayMaps["Homicidio"] = homicidios;
 	overlayMaps["Furto de Celular"] = furtosCelular;
-	overlayMaps["Roubo de Veiculo"] = roubosVeiculo;
-	overlayMaps["Furto de Veiculo"] = furtosVeiculo;
+	overlayMaps["Furto de Veículo"] = furtosVeiculo;
+	overlayMaps["Homicidio"] = homicidios;
 	overlayMaps["Latrocínio"] = latrocinios;
 	overlayMaps["Lesão Seguida de Morte"] = lesaoMorte;
+	overlayMaps["Morte por Intervenção Policial"] = lesaoMorte;
+	overlayMaps["Roubo de Celular"] = roubosCelular;
+	overlayMaps["Roubo de Veiculo"] = roubosVeiculo;
 	
 	L.control.layers(null, overlayMaps).addTo(mymap);
 });
