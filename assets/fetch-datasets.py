4a9f38d917f10af2bf7c226c3b2c6a231c127c7f
Ricardo Martinelli de Oliveira <rmartine@redhat.com>
2019-12-26 22:21:21 -0300

    new version

--- assets/fetch-datasets.py
+++ assets/fetch-datasets.py
@@ -1,5 +1,7 @@
-from scrapy.http import FormRequest
+# -*- coding: utf-8 -*-
+from scrapy.http import FormRequest, TextResponse
 from scrapy.spiders import Spider
+import logging
 
 '''
 Scrapy spider que captura dados de crime da SSP (Secretaria
@@ -11,107 +13,113 @@ https://stackoverflow.com/questions/28974838/crawling-through-pages-with-postbac
 https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#std:setting-RETRY_ENABLED
 '''
 
+# Esses cabecalhos sao obrigatorios por conta da forma como a pagina foi desenvolvida
 HEADERS = {
-    'X-MicrosoftAjax': 'Delta=true',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36'
+  'X-MicrosoftAjax': 'Delta=true',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36'
 }
 
 BASE_PATH = "/run/media/rmartine/TOSHIBA EXT/big-data-projects/ssp" # Preencha esse valor com um caminho para gravar os arquivos xls.
 
+logger = logging.getLogger('ssp_logger')
+
 class SegurancaPublicaSpider(Spider):
-    name = 'ssp spider'
-    start_urls = ['http://www.ssp.sp.gov.br/transparenciassp/Consulta.aspx']
-    crimes = [
-        'FurtoCelular'
-        'FurtoVeiculo',
-        #'Homicicio'
-        #'Latrocinio',
-        #'LesaoMorte',
-        #'MortePolicial',
-        #'MorteSuspeita',
-        #'RouboCelular',
-        #'RouboVeiculo'
-    ]
-    download_delay = 1.5
-    download_timeout = 1080
-    years = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17']
-    months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+  name = 'ssp spider'
+  start_urls = ['http://www.ssp.sp.gov.br/transparenciassp/Consulta.aspx']
+  crimes = [
+    'Feminicidio'
+    'FurtoCelular'
+    'FurtoVeiculo',
+    'Homicicio',
+    'Latrocinio',
+    'LesaoMorte',
+    'MortePolicial',
+    'MorteSuspeita',
+    'RouboCelular',
+    'RouboVeiculo'
+  ]
+  download_delay = 1.5
+  download_timeout = 1080
+  log_level='INFO'
+  years = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19']
+  months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 
-    def parse(self, response):
-        for crime in self.crimes:
-            yield FormRequest(
-                self.start_urls[0],
-                method='POST',
-                formdata={
-                    '__EVENTTARGET': "ctl00$cphBody$btn{}".format(crime),
-                    '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first(),
-                    '__EVENTVALIDATION': response.css('input#__EVENTVALIDATION::attr(value)').extract_first(),
-                },
-                dont_filter=True,
-                meta={'crime': crime},
-                headers=HEADERS,
-                callback=self.parse_years
-            )
+  def __init__(self, *args, **kwargs):
+    logger.setLevel(logging.INFO)
+    super().__init__(*args, **kwargs)
 
-    def parse_years(self, response):
-        for year in self.years:
-            crime = response.meta.get('crime')
-            yield FormRequest(
-				self.start_urls[0],
-                method='POST',
-				formdata = {
-					'__EVENTTARGET': 'ctl00$cphBody$lkAno' + year,
-					'__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first(),
-                    '__EVENTVALIDATION': response.css('input#__EVENTVALIDATION::attr(value)').extract_first(),
-				},
-                dont_filter=True,
-                meta={'crime': crime, 'year': year},
-                headers=HEADERS,
-				callback=self.parse_months
-			)
+  def parse(self, response):
+    for crime in self.crimes:
+      yield FormRequest(
+        self.start_urls[0],
+        method='POST',
+        formdata={
+          '__EVENTTARGET': "ctl00$cphBody$btn{}".format(crime),
+          '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first(),
+          '__EVENTVALIDATION': response.css('input#__EVENTVALIDATION::attr(value)').extract_first(),
+        },
+        dont_filter=True,
+        meta={'crime': crime},
+        headers=HEADERS,
+        callback=self.parse_years)
 
-    def parse_months(self, response):
-        for month in self.months:
-            crime = response.meta.get('crime')
-            year = response.meta.get('year')
-            yield FormRequest(
-				self.start_urls[0],
-                method='POST',
-				formdata = {
-					'__EVENTTARGET': 'ctl00$cphBody$lkMes' + month,
-					'__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first(),
-                    '__EVENTVALIDATION': response.css('input#__EVENTVALIDATION::attr(value)').extract_first(),
-				},
-                dont_filter=True,
-                meta={'crime': crime, 'year': year, 'month': month},
-                headers=HEADERS,
-				callback=self.parse_export
-			)
+  def parse_years(self, response):
+    for year in self.years:
+      crime = response.meta.get('crime')
+      yield FormRequest(
+        self.start_urls[0],
+        method='POST',
+        formdata = {
+          '__EVENTTARGET': 'ctl00$cphBody$lkAno' + year,
+          '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first(),
+          '__EVENTVALIDATION': response.css('input#__EVENTVALIDATION::attr(value)').extract_first(),
+        },
+        dont_filter=True,
+        meta={'crime': crime, 'year': year},
+        headers=HEADERS,
+        callback=self.parse_months)
 
-    def parse_export(self, response):
-        crime = response.meta.get('crime')
-        year = response.meta.get('year')
-        month = response.meta.get('month')
-        yield FormRequest(
-			self.start_urls[0],
-            method='POST',
-			formdata = {
-				'__EVENTTARGET': 'ctl00$cphBody$ExportarBOLink',
-				'__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first(),
-                '__EVENTVALIDATION': response.css('input#__EVENTVALIDATION::attr(value)').extract_first(),
-			},
-            dont_filter=True,
-            meta={'crime': crime, 'year': year, 'month': month},
-            headers=HEADERS,
-			callback=self.parse_result
-		)
+  def parse_months(self, response):
+    for month in self.months:
+      crime = response.meta.get('crime')
+      year = response.meta.get('year')
+      yield FormRequest(
+        self.start_urls[0],
+        method='POST',
+        formdata = {
+          '__EVENTTARGET': 'ctl00$cphBody$lkMes' + month,
+          '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first(),
+          '__EVENTVALIDATION': response.css('input#__EVENTVALIDATION::attr(value)').extract_first(),
+        },
+        dont_filter=True,
+        meta={'crime': crime, 'year': year, 'month': month},
+        headers=HEADERS,
+        callback=self.parse_export)
 
-    def parse_result(self, response):
-        crime = response.meta.get('crime')
-        year = format(int(response.meta.get('year')), '02d')
-        month = format(int(response.meta.get('month')), '02d')
-        filename = "{}/raw/20{}/{}_20{}_{}.xls".format(BASE_PATH, year, crime, year, month)
-        print("Writing file {} now!".format(filename))
-        with open(filename, 'w+') as csv_file:
-            csv_file.write(response.body)
-            csv_file.close
+  def parse_export(self, response):
+    crime = response.meta.get('crime')
+    year = response.meta.get('year')
+    month = response.meta.get('month')
+    yield FormRequest(
+      self.start_urls[0],
+      method='POST',
+      formdata = {
+        '__EVENTTARGET': 'ctl00$cphBody$ExportarBOLink',
+        '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first(),
+        '__EVENTVALIDATION': response.css('input#__EVENTVALIDATION::attr(value)').extract_first(),
+      },
+      dont_filter=True,
+      meta={'crime': crime, 'year': year, 'month': month},
+      headers=HEADERS,
+      callback=self.parse_result)
+
+  def parse_result(self, response):
+    crime = response.meta.get('crime')
+    year = format(int(response.meta.get('year')), '02d')
+    month = format(int(response.meta.get('month')), '02d')
+    filename = "{}/raw/20{}/{}_20{}_{}.xls".format(BASE_PATH, year, crime, year, month)
+    print("Writing file {} now!".format(filename))
+    with open(filename, 'w+') as csv_file:
+      body = TextResponse(response.url, body=response.body)
+      csv_file.write(body.text)
+      csv_file.close
--- assets/prepare-data.sh
+++ assets/prepare-data.sh
@@ -2,19 +2,20 @@
 
 ###################################
 # Descobrir caractere hexa no vim: ga
-# substituir caractere hexa no vim: :%s/\%x<hex-value>//g
+# Substituir caractere hexa no vim: :%s/\%x<hex-value>//g
 #######################################
 
 BASE_PATH="`pwd`" # Preencha esse valor com um caminho para ler os arquivos xls e converter para csv.
 
 declare -a crimes=(
+	'Feminicidio'
 	'FurtoCelular'
 	'FurtoVeiculo'
 	'Homicicio'
 	'Latrocinio'
 	'LesaoMorte'
 	'MortePolicial'
-	#'MorteSuspeita'
+	'MorteSuspeita'
 	'RouboCelular'
 	'RouboVeiculo'
 );
@@ -35,6 +36,8 @@ declare -a years=(
 	'2015'
 	'2016'
 	'2017'
+	'2018'
+	'2019'
 );
 
 for crime in ${crimes[@]}; do
@@ -48,39 +51,25 @@ for crime in ${crimes[@]}; do
 
 			lcrime=$(echo $crime | tr '[:upper:]' '[:lower:]')
 
-			cp -v $crime\_$year\_$month.xls ../../wrangled/$year/$lcrime\_$year\_$month.tsv
+			if [ -f $crime\_$year\_$month.xls ]; then
+				cp -v $crime\_$year\_$month.xls ../../wrangled/$year/$lcrime\_$year\_$month.tsv
 
-			pushd ../../wrangled/$year > /dev/null
-			sed -i 's/\x00//g' $lcrime\_$year\_$month.tsv
-			sed -i 's/,//g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xc7/C/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xfa/u/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xed/i/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xf7/o/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xf4/o/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xe3/a/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xc3/A/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xc1/A/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xc2/A/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xcd/I/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xe0/a/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xe9/e/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xc9/E/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xca/E/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xea/e/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xc9/E/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xd3/O/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xd4/O/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xd5/O/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xda/U/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xaa/a./g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xe7/c/g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xa7/par. /g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\xba/o./g' $lcrime\_$year\_$month.tsv
-			sed -i 's/\"//g' $lcrime\_$year\_$month.tsv
-			cat $lcrime\_$year\_$month.tsv | tr '\t' ',' >  $lcrime\_$year\_$month.csv
-			rm -rf $lcrime\_$year\_$month.tsv
-			popd > /dev/null
+				pushd ../../wrangled/$year > /dev/null
+				sed -i 's/\x00//g' $lcrime\_$year\_$month.tsv
+				sed -i 's/\"//g' $lcrime\_$year\_$month.tsv
+				cat $lcrime\_$year\_$month.tsv | tr '\t' ',' >  $lcrime\_$year\_$month.csv
+
+				# Descobre o encoding atual do arquivo para assim converter para UTF-8
+				ENCODING=$(file -i $lcrime\_$year\_$month.csv | cut -d ';' -f2 | cut -d '=' -f2 | tr '[:lower:]' '[:upper:]')
+				iconv -f $ENCODING -t UTF-8 $lcrime\_$year\_$month.csv -o $lcrime\_$year\_$month.csv.converted
+
+				# Efetua o processo de conversao para UTF-8, remove o arquivo antigo para ficar com o novo
+				rm -rf $lcrime\_$year\_$month.csv
+				mv $lcrime\_$year\_$month.csv.converted $lcrime\_$year\_$month.csv
+
+				rm -rf $lcrime\_$year\_$month.tsv
+				popd > /dev/null
+			fi
 		done
 		popd > /dev/null
 	done
